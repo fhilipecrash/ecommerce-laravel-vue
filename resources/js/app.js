cfc1cce59704f3154ccf93fb6cb4ce96d03d49f5
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-08-19 22:28:48 -0300

    update: remove dotenv dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,9 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { InertiaProgress } from '@inertiajs/progress'
-import * as Dotenv from 'dotenv'
 
 import '../css/app.css'
-
-Dotenv.config()
 
 InertiaProgress.init({
   color: '#fff',
@@ -16,6 +13,8 @@ createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
+      .component('Link', Link)
+      .component('Head', Head)
       .use(plugin)
       .mount(el)
   },
