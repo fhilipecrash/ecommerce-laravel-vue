c7ead34e938fb7131759b0edfaf04b9ec355534e
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-08-22 22:32:59 -0300

    feat: add prettier and editorconfig files

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -1,40 +1,38 @@
 <script lang="ts">
+  export default {
+    setup() {
+      const form = reactive({
+        name: null,
+        email: null,
+        password: null,
+      })
 
-export default {
-  setup() {
-    const form = reactive({
-      name: null,
-      email: null,
-      password: null,
-    });
+      function handleForm() {
+        Inertia.post('/register', form)
+      }
 
-    function handleForm() {
-      Inertia.post('/register', form);
-    }
-
-    return { form, handleForm }
-  },
-  props: {
-    errors: {
-      type: Object,
+      return { form, handleForm }
     },
-  },
-}
+    props: {
+      errors: {
+        type: Object,
+      },
+    },
+  }
 </script>
 
 <template>
   <div class="h-screen w-screen flex flex-col items-center justify-center">
     <form class="flex flex-col items-center" @submit.prevent="handleForm()">
-      <input type="text" placeholder="Username" v-model="form.name">
+      <input type="text" placeholder="Username" v-model="form.name" />
       <span> {{ errors.name }} </span>
-      <input type="email" placeholder="Email" v-model="form.email">
+      <input type="email" placeholder="Email" v-model="form.email" />
       <span> {{ errors.email }} </span>
-      <input type="password" placeholder="Password" v-model="form.password">
+      <input type="password" placeholder="Password" v-model="form.password" />
       <span> {{ errors.password }} </span>
       <button
         type="submit"
-        class="text-white border-2 h-10 w-20 rounded-md transition-colors hover:bg-white hover:text-slate-800 hover:border-none"
-      >
+        class="text-white border-2 h-10 w-20 rounded-md transition-colors hover:bg-white hover:text-slate-800 hover:border-none">
         Sign up
       </button>
     </form>
@@ -42,10 +40,10 @@ export default {
 </template>
 
 <style lang="postcss" scoped>
-input {
-  @apply border-2 border-gray-600 bg-gray-600 m-1 rounded text-white p-3;
-}
-span {
-  @apply text-red-600;
-}
+  input {
+    @apply border-2 border-gray-600 bg-gray-600 m-1 rounded text-white p-3;
+  }
+  span {
+    @apply text-red-600;
+  }
 </style>
