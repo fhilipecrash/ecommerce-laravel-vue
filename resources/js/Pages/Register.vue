8caa17f82cfd9d4e13c534c28088810e15d11718
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2022-08-23 22:31:07 +0000

    refactor: change to script setup syntax

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -1,35 +1,34 @@
-<script lang="ts">
-  export default {
-    setup() {
-      const form = reactive({
-        name: null,
-        email: null,
-        password: null,
-      })
+<script lang="ts" setup>
+  const props = defineProps<{
+    errors: {
+      name: string
+      email: string
+      password: string
+    }
+  }>()
 
-      function handleForm() {
-        Inertia.post('/register', form)
-      }
+  const form = reactive({
+    name: null,
+    email: null,
+    password: null,
+  })
 
-      return { form, handleForm }
-    },
-    props: {
-      errors: {
-        type: Object,
-      },
-    },
+  function handleForm() {
+    Inertia.post('/register', form)
   }
+
+  return { form, handleForm }
 </script>
 
 <template>
   <div class="h-screen w-screen flex flex-col items-center justify-center">
     <form class="flex flex-col items-center" @submit.prevent="handleForm()">
       <input type="text" placeholder="Username" v-model="form.name" />
-      <span> {{ errors.name }} </span>
+      <span> {{ props.errors.name }} </span>
       <input type="email" placeholder="Email" v-model="form.email" />
-      <span> {{ errors.email }} </span>
+      <span> {{ props.errors.email }} </span>
       <input type="password" placeholder="Password" v-model="form.password" />
-      <span> {{ errors.password }} </span>
+      <span> {{ props.errors.password }} </span>
       <button
         type="submit"
         class="text-white border-2 h-10 w-20 rounded-md transition-colors hover:bg-white hover:text-slate-800 hover:border-none">
